6ca81b9c300abd42321dd94452f8fb78b04c0aae
Shaheen Karodia <karodia.shaheen@gmail.com>
2021-06-03 15:27:11 +0200

    added bulma and node sass

--- src/components/top-navbar/TopNavbar.js
+++ src/components/top-navbar/TopNavbar.js
@@ -1,8 +1,18 @@
 import React from "react";
+import "./top-navbar.scss";
 import PropTypes from "prop-types";
 
 function TopNavbar(props) {
-  return <div>TopNavbar</div>;
+  return (
+    <div className="top-navbar">
+      <div class="columns">
+        <div class="column">First column</div>
+        <div class="column">Second column</div>
+        <div class="column">Third column</div>
+        <div class="column">Fourth column</div>
+      </div>
+    </div>
+  );
 }
 
 TopNavbar.propTypes = {};
