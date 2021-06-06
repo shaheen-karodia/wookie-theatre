90affc752c613dd02014e8c00c9db9bf86146c63
Shaheen Karodia <karodia.shaheen@gmail.com>
2021-06-06 10:57:32 +0200

    styled and created all components for top navbar

--- src/components/top-navbar/TopNavbar.js
+++ src/components/top-navbar/TopNavbar.js
@@ -5,12 +5,33 @@ import PropTypes from "prop-types";
 function TopNavbar(props) {
   return (
     <div className="top-navbar">
-      <input
-        type="text"
-        placeholder="Search name"
-        // value={search}
-        // onChange={(e) => setSearch(e.target.value)}
-      />
+      <div className="logo-wrapper">
+        <img
+          className="logo-icon"
+          src="/images/logo/chewie_icon.svg"
+          alt="Chewie Icon"
+        />
+        <img
+          className="logo-text"
+          src="/images/logo/logo_text.svg"
+          alt="Wookie Movies"
+        />
+      </div>
+
+      <div className="search-wrapper">
+        <img
+          className="search-icon"
+          src="/images/icons/search_icon.svg"
+          alt="Search Icon"
+        />
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Search title..."
+          // value={search}
+          // onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
     </div>
   );
 }
