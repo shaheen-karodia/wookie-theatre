08330cab7d77f5deea00b7d902563602013039c9
Shaheen Karodia <karodia.shaheen@gmail.com>
2021-06-06 17:19:53 +0200

    initial working carousel

--- src/components/home-page/HomePage.js
+++ src/components/home-page/HomePage.js
@@ -1,8 +1,8 @@
 import React from "react";
-import PropTypes from "prop-types";
+import MovieCarousel from "./MovieCarousel";
 
 function HomePage(props) {
-  return <div>Home Page</div>;
+  return <MovieCarousel />;
 }
 
 HomePage.propTypes = {};
